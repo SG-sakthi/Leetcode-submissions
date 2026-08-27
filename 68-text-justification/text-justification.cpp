281c64c89ea184acc7addd50c8c655f397dcff18
Sakthi Ganesh <96302038+SG-sakthi@users.noreply.github.com>
2026-08-27 23:12:23 +0530

    Time: 0 ms (100.00%) | Memory: 10.7 MB (69.00%) - LeetSync

---
+++ 68-text-justification/text-justification.cpp
@@ -0,0 +1,66 @@
+class Solution {
+public:
+    vector<string> fullJustify(vector<string>& words, int maxWidth) {
+
+        vector<string> result;
+        int n = words.size();
+        int i = 0;
+
+        while (i < n) {
+
+            int j = i;
+            int lettersCount = 0;
+
+            while (j < n &&
+                   lettersCount + words[j].size() + (j - i) <= maxWidth) {
+
+                lettersCount += words[j].size();
+                j++;
+            }
+
+            int gaps = j - i - 1;
+            string line;
+
+     
+            if (j == n || gaps == 0) {
+
+                line += words[i];
+
+                for (int k = i + 1; k < j; k++) {
+                    line += " ";
+                    line += words[k];
+                }
+                line += string(maxWidth - line.size(), ' ');
+            }
+
+            else {
+
+                int totalSpaces = maxWidth - lettersCount;
+
+                int evenSpaces = totalSpaces / gaps;
+                int extraSpaces = totalSpaces % gaps;
+
+                for (int k = i; k < j - 1; k++) {
+
+                    line += words[k];
+
+                    line += string(
+                        evenSpaces + (extraSpaces > 0 ? 1 : 0),
+                        ' '
+                    );
+
+                    if (extraSpaces > 0)
+                        extraSpaces--;
+                }
+                line += words[j - 1];
+            }
+
+            result.push_back(line);
+
+    
+            i = j;
+        }
+
+        return result;
+    }
+};
